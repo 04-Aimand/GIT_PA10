899edab15f5f9d2adb50a9bfab92662d0aabd314
04-Aimand <heera.aimand@gmail.com>
2023-10-17 15:33:35 +0800

    Edited Unity Project
    Implemented scoring system, jump, lose condition and lose scene

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,10 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Player : MonoBehaviour
 {
     private Animation thisAnimation;
+    public Rigidbody rb;
+
+    private float jumpForce = 7f;
+    private int score;
+    public Text scoreText;
 
     void Start()
     {
@@ -15,6 +22,34 @@ public class Player : MonoBehaviour
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space))
+        {
             thisAnimation.Play();
+            rb.velocity = Vector2.up * jumpForce;
+            
+        }
+
+        transform.position = new Vector3(0, Mathf.Clamp(transform.position.y, -10, 6), 0);
+
+        if(transform.position.y < -6)
+        {
+            SceneManager.LoadScene("GameOver");
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Obstacle")
+        {
+            SceneManager.LoadScene("GameOver");
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.gameObject.tag == "ScoreTrigger")
+        {
+            score++;
+            scoreText.text = "SCORE : " + score;
+        }
     }
 }
